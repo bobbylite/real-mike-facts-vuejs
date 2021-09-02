fbebb27e757f073e65df073bcb11cfccc61e3b75
Robert Luisi <bobby.luisi@gmail.com>
2021-09-02 02:36:46 -0400

    production devops

---
+++ devops/content/README.md
@@ -0,0 +1,12 @@
+# real-mike-facts
+
+## Compiled with Vue.js
+#### Support the vue-cli here [cli.vuejs.org](https://cli.vuejs.org/)
+#### Support Vue.js here [vuejs.org](https://vuejs.org/)
+
+## Hosted with Pages
+#### Support github pages here [pages.github.com](https://pages.github.com/)
+
+## Real Mike Facts developer information
+#### Active github page - [bobbylite.github.io/realmikefacts](https://bobbylite.github.io/realmikefacts/)
+#### Active github git repo - [github.com/bobbylite/real-mike-facts-vuejs](https://github.com/bobbylite/real-mike-facts-vuejs)
---
+++ devops/deploy-to-gh-pages.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+## if you want to add a branch content to your local branch 
+# cp ../devops/content/<GIT-CONTENT>.* ./
+cp ../devops/content/README.md ./
+
+git init
+git add -A
+git commit -m 'deploy'
+git remote add origin https://github.com/bobbylite/realmikefacts
+git push --set-upstream origin master -f
+
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+echo '\nSuccessful deployment to https://bobbylite.github.io/realmikefacts\n'
+
+cd -
