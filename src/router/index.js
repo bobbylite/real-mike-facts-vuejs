b5390857b6c92e01c5680be2f92e1a4100228eff
Robert Luisi <bobby.luisi@gmail.com>
2021-09-02 10:58:37 -0400

    fixed router paths for github pages production

--- src/router/index.js
+++ src/router/index.js
@@ -3,34 +3,33 @@ import Test from "@/components/Test.vue";
 import Login from "@/components/Login.vue";
 import { postData, getData, putData, optionsData } from "../http/request";
 
-const routes = [
-  {
-    path: "/",
-    name: "Login",
-    component: Login,
-    props: {
-        post: postData,
-        get: getData,
-        put: putData,
-        options: optionsData,
-    }
-  },
-  {
-    path: "/",
-    name: "Test",
-    component: Test,
-    props: {
-        post: postData,
-        get: getData,
-        put: putData,
-        options: optionsData,
-    }
-  },
+const routes = [{
+        path: "/realmikefacts",
+        name: "Login",
+        component: Login,
+        props: {
+            post: postData,
+            get: getData,
+            put: putData,
+            options: optionsData,
+        }
+    },
+    {
+        path: "/realmikefacts",
+        name: "Test",
+        component: Test,
+        props: {
+            post: postData,
+            get: getData,
+            put: putData,
+            options: optionsData,
+        }
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
