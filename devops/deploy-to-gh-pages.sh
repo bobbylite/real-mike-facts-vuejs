8ea083e2dfe75dfcb2812f0e17f4b94c0716b665
bobbylite <bobby.luisi@gmail.com>
2022-03-02 00:52:40 -0500

    Revert "production deployment"
    This reverts commit e5b26f8c65817a9c0663c0c37a07c606aba06de4.

--- devops/deploy-to-gh-pages.sh
+++ devops/deploy-to-gh-pages.sh
@@ -16,15 +16,11 @@ cd dist
 # cp ../devops/content/<GIT-CONTENT>.* ./
 cp ../devops/content/README.md ./
 
-#git init
-#git add -A
-#git commit -m 'deploy'
-#git remote add origin https://github.com/bobbylite/realmikefacts
-#git push --set-upstream origin develop
-
-git add --all
-git commit -m 'production deployment'
-git push
+git init
+git add -A
+git commit -m 'deploy'
+git remote add origin https://github.com/bobbylite/realmikefacts
+git push --set-upstream origin master -f
 
 
 # if you are deploying to https://<USERNAME>.github.io
